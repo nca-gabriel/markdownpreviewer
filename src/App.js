79cbe99404c717f47a047b4b071850b87f0bf8f3
Neil <ncagabriel02@gmail.com>
2025-09-04 12:07:54 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import defaultMarkdown from "./defaultMark";
+import { marked } from "marked";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [markdown, setMarkdown] = useState(defaultMarkdown);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main id="markdown-previewer">
+      <section id="editor-wrapper">
+        <h2>Editor</h2>
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+      </section>
+
+      <section>
+        <h2 id="preview-h2">Preview</h2>
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{
+            __html: marked.parse(markdown, { breaks: true }),
+          }}
+        />
+      </section>
+    </main>
   );
 }
 
---
+++ src/defaultMark.js
@@ -0,0 +1,27 @@
+const defaultMarkdown = `
+# Welcome to my Markdown Previewer!
+
+## This is a sub-heading
+
+You can make text **bold** or *italic*.
+
+You can also do \`inline code\`.
+
+\`\`\`js
+// code block
+function hello() {
+  console.log("Hello Markdown!");
+}
+\`\`\`
+
+> Blockquote example
+
+- List item 1
+- List item 2
+
+[FreeCodeCamp](https://www.freecodecamp.org)
+
+![Image](https://picsum.photos/200)
+`;
+
+module.exports = defaultMarkdown;
